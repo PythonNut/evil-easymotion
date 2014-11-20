74aae14c30271dc0af49fa70547fb69a81cfafdb
PythonNut <PythonNut@users.noreply.github.com>
2014-11-20 17:10:19 +0000

    Moar package boilerplate

--- evil-easymotion.el
+++ evil-easymotion.el
@@ -1,9 +1,37 @@
+;;; evil-easymotion.el --- A port of vim's easymotion to evil
+
+;; Copyright (C) 2014 PythonNut
+
+;; Author: PythonNut <pythonnut@pythonnut.com>
+;; Keywords: convenience, evil
+;; Version: 20141120
+;; URL: https://github.com/pythonnut/evil-easymotion.el
+;; Package-Requires: ((emacs "24") (evil "20141119.205") (cl-lib "0.5") (ace-jump-mode "20140616.115"))
+
+;;; License:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+
+;;; Code:
 (require 'cl-lib)
 (eval-when-compile
   (progn
     (require 'names)
     (require 'noflet)
-    (require 'cl-lib)
     (require 'ace-jump-mode)
     (require 'evil)))
 
@@ -131,3 +159,5 @@
     (ease-motion (kbd-pfx "s U") evil-backward-up-sexp)
     (ease-motion (kbd-pfx "s n") evil-next-sexp)
     (ease-motion (kbd-pfx "s p") evil-previous-sexp)))
+
+;;; evil-easymotion.el ends here
