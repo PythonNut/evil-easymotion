b51c4bc04be87bfc15ef7b7a597af82742700b06
PythonNut <PythonNut@users.noreply.github.com>
2014-11-29 06:02:30 +0000

    Add commentary

--- evil-easymotion.el
+++ evil-easymotion.el
@@ -1,4 +1,4 @@
-;;; evil-easymotion.el --- A port of vim's easymotion to evil
+;;; evil-easymotion.el --- A port of vim's easymotion to emacs
 
 ;; Copyright (C) 2014 PythonNut
 
@@ -24,7 +24,34 @@
 ;; along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
+;; This is a clone of the popular easymotion package for vim, which
+;; describes itself in these terms:
 
+;; > EasyMotion provides a much simpler way to use some motions in vim.
+;; > It takes the <number> out of <number>w or <number>f{char} by
+;; > highlighting all possible choices and allowing you to press one key
+;; > to jump directly to the target.
+
+;; If you're having trouble picturing this, please visit the github repo
+;; for a screencast.
+
+;; Usage/status
+;; ============
+
+;; evil-easymotion, rather unsurprisingly can use evil. However, you don't
+;; _need_ evil to use it. evil-easymotion can happily define motions for
+;; regular emacs commands. With that said, evil is recommended, not
+;; least because it's awesome.
+
+;; Currently most motions are supported, and it's easy to define your own easymotions.
+
+;;   (evilem-define (kbd "SPC w") evil-forward-word-begin)
+
+;; To define easymotions for all motions that evil defines by default, add
+
+;;   (evilem-default-keybindings "SPC")
+
+;; This binds all motions under the prefix `SPC` in `evil-motion-state-map`. This is not done by default for motions defined manually. You will need to supply the prefix.
 
 ;;; Code:
 (require 'cl-lib)
